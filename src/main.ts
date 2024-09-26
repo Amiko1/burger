b2de35a3b702b19d37fb8a5ccc0bd995a55c03e6
Amirani <amiraniburjanadze@gmail.com>
2024-09-27 02:07:47 +0400

    feat: layout chipses

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 import "./reset.css";
 import "./style.css";
 import "./animate.css";
+
+const closedBurger = document.querySelector(
+  ".burger__closed"
+) as unknown as HTMLDivElement;
+const openedBurger = document.querySelector(
+  ".burger__opened"
+) as unknown as HTMLDivElement;
+closedBurger.addEventListener("click", () => {
+  closedBurger.classList.add("display-none");
+  openedBurger.classList.add("display-block");
+});
