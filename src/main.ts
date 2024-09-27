db0de4aac80e935b3a3def70c8ab7688bc1531f6
amirani <amirani>
2024-09-27 18:08:43 +0400

    feat: remove initial transitions

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,38 @@ import "./reset.css";
 import "./style.css";
 import "./animate.css";
 
-const openedBurger = document.querySelector(
+const openedBurgers = document.querySelectorAll(
   ".burger__opened"
 ) as unknown as HTMLDivElement;
 const burgerIngredients = document.querySelectorAll(".burger__igredient");
 const burgerDescriptions = document.querySelectorAll(".burger__description");
+const burgerSectionHeaders = document.querySelectorAll(
+  ".burger-section__header"
+);
 
-openedBurger.addEventListener("click", () => {
+setTimeout(() => {
   for (let burgerIngredient of burgerIngredients) {
-    burgerIngredient.classList.toggle("transform-normal");
+    burgerIngredient.classList.remove("no-transition");
   }
   for (let burgerDescription of burgerDescriptions) {
-    burgerDescription?.classList.toggle("display-block");
+    burgerDescription.classList.remove("no-transition");
   }
-});
+}, 0);
+
+for (let openedBurger of openedBurgers) {
+  openedBurger.addEventListener("click", () => {
+    for (let burgerIngredient of burgerIngredients) {
+      burgerIngredient.classList.toggle("transform-normal");
+    }
+    for (let burgerDescription of burgerDescriptions) {
+      burgerDescription?.classList.toggle("hide-out");
+    }
+
+    console.log(burgerSectionHeaders);
+
+    for (let burgetDesctionHeader of burgerSectionHeaders) {
+      console.log(burgetDesctionHeader);
+      burgetDesctionHeader?.classList.toggle("throw-up");
+    }
+  });
+}
