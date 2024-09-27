5f4b8b56fd9d606d140b8cd90b98865f209dc29e
Amirani <amiraniburjanadze@gmail.com>
2024-09-27 14:40:37 +0400

    feat: close and open bburger

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import "./reset.css";
 import "./style.css";
 import "./animate.css";
 
-const closedBurger = document.querySelector(
-  ".burger__closed"
-) as unknown as HTMLDivElement;
 const openedBurger = document.querySelector(
   ".burger__opened"
 ) as unknown as HTMLDivElement;
-closedBurger.addEventListener("click", () => {
-  closedBurger.classList.add("display-none");
-  openedBurger.classList.add("display-block");
+const burgerIngredients = document.querySelectorAll(".burger__igredient");
+
+openedBurger.addEventListener("click", () => {
+  for (let burgerIngredient of burgerIngredients) {
+    burgerIngredient.classList.toggle("transform-normal");
+  }
 });
