bf8697f9bb28220f23e853c33754e1ab714aeb33
amirani <amirani>
2024-09-27 16:59:41 +0400

    feat: animate ingredient descriptions

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ const openedBurger = document.querySelector(
   ".burger__opened"
 ) as unknown as HTMLDivElement;
 const burgerIngredients = document.querySelectorAll(".burger__igredient");
+const burgerDescriptions = document.querySelectorAll(".burger__description");
 
 openedBurger.addEventListener("click", () => {
   for (let burgerIngredient of burgerIngredients) {
     burgerIngredient.classList.toggle("transform-normal");
+  }
+  for (let burgerDescription of burgerDescriptions) {
+    burgerDescription?.classList.toggle("display-block");
   }
 });
